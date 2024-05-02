968e8c3e13101b0e9998f2853266dd095215d020
24k-Baller <davidhassan194@gmail.com>
2024-05-02 09:14:26 +0100

    first commit

---
+++ java/dice.js
@@ -0,0 +1,24 @@
+// function game() {
+//     let RandomNumber1 = (Math.random()*6)+1;
+//     let RandomNumber2 = (Math.random()*6)+1;
+    
+    
+//     let player1 = parseInt(RandomNumber1);
+//     let player2 = parseInt(RandomNumber2);
+    
+    
+//     document.querySelector("#Player1").innerHTML = player1; 
+//     document.querySelector("#Player2").innerHTML = player2;
+    
+//     if(player1 >player2){
+//         document.getElementById("result").innerHTML = "player 1 win";
+//     }
+//     else if(player2 > player1){
+//         document.getElementById("result").innerHTML = "player 2 win";
+
+        
+//     }
+//     else{
+//         document.getElementById("result").innerHTML = "players draw";
+//     }
+// }
